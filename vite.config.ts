bb7459f0e0454e92c1d28124e65a75688e1d694d
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-02 21:55:53 +0530

    patch: remove mkcert/https support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [mkcert(), sveltekit()],
+  plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
